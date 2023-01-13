9e6772a0caa97f92be0951f7b65878d535e94d50
lilia <lilianom59@outlook.fr>
2023-01-13 12:39:36 +0100

    dates

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 // Premiere ligne
+$today = date("d");
+echo $today . "<br>";
 
+$today1 = date("Y.m.d");
+echo $today1 . "<br>";
 
-
+$today2 = date("H:i:s");
+echo $today2 . "<br>";
 
 
 
